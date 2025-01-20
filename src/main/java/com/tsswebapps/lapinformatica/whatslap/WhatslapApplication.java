541af11ac6aa16f79ac274ea82b622201779d5b7
thiago-santanna <uzmaki_thiago@outlook.com>
2025-01-20 15:31:42 -0300

    primeiro commit

---
+++ src/main/java/com/tsswebapps/lapinformatica/whatslap/WhatslapApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.lapinformatica.whatslap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatslapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatslapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsswebapps/lapinformatica/whatslap/WhatslapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.lapinformatica.whatslap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatslapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
